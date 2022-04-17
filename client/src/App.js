c053f92ffef743868db17aa6788407ef0987e804
dhan-ush <nagadhanushkv1990@gmail.com>
2022-04-17 13:19:44 +0530

    merge conflicts sorted

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,11 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
-import React from "react"
-import Homepage from './pages/homepage';
-import Userlogin from './pages/userLogin';
-import UserReg from './pages/userReg';
-import Dashboard from './pages/dashboard';
-=======
 import React from "react";
 import Homepage from "./pages/homepage";
 import Userlogin from "./pages/userLogin";
 import UserReg from "./pages/userReg";
 import Registration from "./pages/registrationDetails";
->>>>>>> 89c7c72ddc50159ba13ed5103d2116ee65517970
+import Dashboard from "./pages/dashboard"
 function App() {
   return (
     <div className="App">
@@ -21,11 +14,8 @@ function App() {
           <Route exact path="/" element={<Homepage />} />
           <Route path="/userlogin" element={<Userlogin />} />
           <Route path="/userReg" element={<UserReg />} />
-<<<<<<< HEAD
           <Route path="/dash" element={<Dashboard />} />
-=======
           <Route path="/registrationDetails" element={<Registration />} />
->>>>>>> 89c7c72ddc50159ba13ed5103d2116ee65517970
         </Routes>
       </Router>
     </div>

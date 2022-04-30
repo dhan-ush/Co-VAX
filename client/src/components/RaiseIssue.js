e682766b76479592ac150bb62d9f46ecba574f10
AnkitSama <88831151+AnkitSama@users.noreply.github.com>
2022-04-30 14:26:14 +0530

    Raise Issue

--- client/src/components/RaiseIssue.js
+++ client/src/components/RaiseIssue.js
@@ -1,55 +1,64 @@
-import React from 'react';
-import st from '../styles/raiseissue.module.css'
-import { useState } from 'react';
-import { Link } from "react-router-dom"
+import React from "react";
+import st from "../styles/raiseissue.module.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-function RaiseIssue({user,setUser}) {
-  const [issueInput,setIssueInput]=useState("");
+function RaiseIssue({ user, setUser, setComp }) {
+  const [issueInput, setIssueInput] = useState("");
 
-  const handleSubmit= (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     console.log(issueInput);
-    if(issueInput.length>200)
-    {
-      alert('Word Limit Exceeded!')
+    if (issueInput.length > 200) {
+      alert("Word Limit Exceeded!");
       return;
-    }
-    else{
-
+    } else {
+      axios
+        .post("http://localhost:3001/AddIssue", {
+          aadhar_number: user.aadhar,
+          issue: issueInput,
+        })
+        .then((response) => {
+          alert("Issue Submitted Successfully");
+          console.log(response.data);
+          setComp(0);
+        });
     }
     // console.log(user.aadhar)
-  }
+  };
   return (
-    <> 
-    <div className={st.outer}>
+    <>
+      <div className={st.outer}>
         <div className={st.outerBox}>
-            <div className={st.title}>
-                Raise an Issue:
-            </div>
-            <div className={st.info}>
-            Raise any issue regarding your experience with our support team (in less than 200 words) 
-            </div>
-            <div className={st.info}>
-             Our team will contact you and resolve them at the earliest. 
-            </div>
-            <div className={st.textbox}>
-              <textarea onChange={ (e) => {
+          <div className={st.title}>Raise an Issue:</div>
+          <div className={st.info}>
+            Raise any issue regarding your experience with our support team (in
+            less than 200 words)
+          </div>
+          <div className={st.info}>
+            Our team will contact you and resolve them at the earliest.
+          </div>
+          <div className={st.textbox}>
+            <textarea
+              onChange={(e) => {
                 setIssueInput(e.target.value);
-              }
-              }
+              }}
               className={st.issueBox}
               placeholder="Briefly Describe your Issue Here.."
-              type="text"  ></textarea>
-            </div>
-            <div className={st.btn}>
-              <div className={st.wordcount}>Word Count:{issueInput.length}</div>
-              <button onClick={handleSubmit} className={st.submitbtn}>Submit</button>
-            </div>
+              type="text"
+            ></textarea>
+          </div>
+          <div className={st.btn}>
+            <div className={st.wordcount}>Word Count:{issueInput.length}</div>
+            <button onClick={handleSubmit} className={st.submitbtn}>
+              Submit
+            </button>
+          </div>
         </div>
-    </div>
-
+      </div>
     </>
-  )
+  );
 }
 
 export default RaiseIssue;
--- client/src/components/viewissues.js
+++ client/src/components/viewissues.js
@@ -4,12 +4,17 @@ import RedIcon from "../assets/cross.png";
 import axios from "axios";
 function ViewIssues() {
   const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.post("http://localhost:3001/DisplayIssues", {}).then((response) => {
+      setData(response.data);
+      console.log(response.data);
+    });
+  }, []);
   return (
     <>
       <div className={st.outer}>
         <div className={st.box}>
-          <div className={st.title1}>User Issues
-          </div>
+          <div className={st.title1}>User Issues</div>
           <div className={st.tableDiv}>
             <table>
               <thead className={st.tableHead}>
@@ -23,7 +28,7 @@ function ViewIssues() {
                   return (
                     <tr className={st.trow}>
                       <td className={st.tcell}>
-                        <div className={st.cells}>{index+1}</div>
+                        <div className={st.cells}>{index + 1}</div>
                       </td>
                       <td className={st.tcell}>
                         <div className={st.cells}>{w.aadhar_number}</div>
